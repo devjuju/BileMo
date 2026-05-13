130f2f0235d9a90a3b66bea8e0c03cd5a6b467c8
devjuju <jleleubellpro1994@gmail.com>
2026-05-13 14:59:14 +0200

    Product pagination

--- app/src/Controller/Api/Product/ListProductController.php
+++ app/src/Controller/Api/Product/ListProductController.php
@@ -5,14 +5,25 @@ namespace App\Controller\Api\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ListProductController extends AbstractController
 {
     #[Route('/api/products', name: 'api_products_list', methods: ['GET'])]
-    public function list(ProductRepository $productRepository): JsonResponse
-    {
-        $products = $productRepository->findAll();
+    public function list(
+        ProductRepository $productRepository,
+        Request $request
+    ): JsonResponse {
+        $page = $request->query->getInt('page', 1);
+        $limit = 5;
+
+        $products = $productRepository->findBy(
+            [],
+            null,
+            $limit,
+            ($page - 1) * $limit
+        );
 
         $data = [];
 
@@ -29,6 +40,10 @@ final class ListProductController extends AbstractController
             ];
         }
 
-        return $this->json($data);
+        return $this->json([
+            'page' => $page,
+            'limit' => $limit,
+            'data' => $data
+        ]);
     }
 }
